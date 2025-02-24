d965f3a0cfcc0834e68c5c334f394f58b65d5e2f
Kishore Varma <kishorevarma2205@gmail.com>
2025-02-24 11:08:53 +0530

    11

--- frontend/myapp/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/postcss7-compat'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
